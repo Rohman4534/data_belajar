3bf7e524052545ac8bc85cc82642b33559fd6b52
Rohman4534 <108682119+Rohman4534@users.noreply.github.com>
2022-12-21 21:01:30 +0800

    Delete tipe data.sql

--- tipe data.sql
+++
@@ -1,5 +0,0 @@
-/* tipe data mysql
-terdapat 2 jenis tipe data di mysql yaitu tipe data number dan character
-ketika mebuat kolom tabel dengan tipe data di mysql. kolom tersebut akan memuat tipe data tersebut.
-jadi kita tidak bisa mengubah tipe data beberapa baris kolom tersebut.
-*/
