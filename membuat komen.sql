3997a3a605d6c615863ca9553994a3bc6f6f4e01
Rohman4534 <108682119+Rohman4534@users.noreply.github.com>
2022-12-21 20:59:49 +0800

    Delete membuat komen.sql

--- membuat komen.sql
+++
@@ -1,7 +0,0 @@
-/* membuat komen
-komen berguna untuk membuat dokumentasi agar pengguna lain bisa lebih mengerti query yang kamu buat
-komentar tidak akan dibaca oleh mysql.ada beberapa simbol untuk membuat komentar */
-
-#  merupakan simbol komen untuk satu baris
-
-/**/   /* merupakan komen untuk multi baris */
